6bac3be62e13671bd4c187114b42bb5ec709d992
Ellen <d54041221@mail.ncku.edu.tw>
2019-08-28 15:46:08 +0800

    test4

--- src/v1/GUI2p.java
+++ src/v1/GUI2p.java
@@ -4,8 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-//123
-//345
+
 
 	public class GUI2p extends JFrame implements ActionListener {
 		public GUI2p() {
