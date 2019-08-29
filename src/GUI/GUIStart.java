db1a9ac2b5a7c15f506108f344f85033907fc323
Fion <N16054661@mail.ncku.edu.tw>
2019-08-29 23:47:35 +0800

    GUIStart

--- src/GUI/GUIStart.java
+++ src/GUI/GUIStart.java
@@ -1,32 +1,116 @@
 package GUI;
 
-import v1.Deck;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIStart {
+import javax.swing.JFrame;
+
+import v1.Deck;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class GUIStart extends JFrame implements ActionListener {
+
+	private static int people;
+	private static int deck;
+	public static void main(String[] args) {
+		GUIStart frame = new GUIStart();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+	public GUIStart() {
+
+		setBounds(100, 100, 400, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JTextField peotextField = new JTextField();
+		peotextField.setBounds(110, 30, 96, 34);
+		getContentPane().add(peotextField);
+		peotextField.setColumns(10);
+
+		
+		
+		JButton btnPeopleButton = new JButton("People button");
+		btnPeopleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String peo = peotextField.getText();
+				people = Integer.valueOf(peo);
+				
+			
+			}
+		});
+		btnPeopleButton.setBounds(210, 30, 96, 23);
+		getContentPane().add(btnPeopleButton);
+		
+		JLabel lblPeopleNumber = new JLabel("People number");
+		lblPeopleNumber.setBounds(10, 30, 90, 15);
+		getContentPane().add(lblPeopleNumber);
+		
+		
+		JTextField decktextField = new JTextField();
+		decktextField.setBounds(110, 74, 96, 34);
+		getContentPane().add(decktextField);
+		decktextField.setColumns(10);
+	
+		
+		
+		JButton btnDeckButton = new JButton("Deck button");
+		btnDeckButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String de = decktextField.getText();
+				deck = Integer.valueOf(de);
+			
+			}
+		});
+		btnDeckButton.setBounds(210, 74, 96, 23);
+		getContentPane().add(btnDeckButton);
+		
+		JLabel lblDeckNumber = new JLabel("Deck number");
+		lblDeckNumber.setBounds(10, 74, 90, 15);
+		getContentPane().add(lblDeckNumber);
+		
+		JButton btnOK = new JButton("OK");
+		btnOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GUIStart.selectGUI();
+				
+			}
+		});
+		btnOK.setBounds(312, 30, 62, 67);
+		getContentPane().add(btnOK);
+	}
 
 	public int numOfpeople() {
-		// Scanner
-		int num = 0;
-		return num;
+		return people;
 	}
 	
 	public int  numOfDeck() {
-		int num = 0;
-		return num;
+		return deck;
 	}
 	
-	public void selectGUI() {
-		GUI gui;
-		switch (numOfpeople()) {
-		case 2:
-			gui = new GUI2p();
-			break;
+	public static void selectGUI() {
+		//System.out.println("Good");
+		switch (people) {
 		case 3:
-			gui = new GUI3p();
+			System.out.println("33");
+			//GUI3p gui3 = new GUI3p();
+			//gui = new GUI3p();
+			break;
+		case 4:
+			System.out.println("44");
+			//GUI4p gui4 = new GUI4p();
+			//gui = new GUI4p();
 			break;
 		}
 
-		gui.startGame(numOfpeople(),numOfDeck());
+		//gui.startGame(numOfpeople(),numOfDeck());
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
