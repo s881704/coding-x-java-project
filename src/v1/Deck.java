c1cef19952fe96f31958b783810f8ec8e9c32d3c
wei <s881704@yahoo.com.tw>
2019-08-30 01:04:10 +0800

    Board function

--- src/v1/Deck.java
+++ src/v1/Deck.java
@@ -1,18 +1,70 @@
 package v1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 	private ArrayList<Card> deck = new ArrayList<Card>();
-	
+
 	public Deck(int n) {
-		//initialize �X�ƵP
+		// initialize 幾副牌
+		this.deck = createDeck(n);
 	}
+
+	// 洗牌
 	public void shuffle() {
-		
+		Collections.shuffle(deck);
 	}
-	public void givecard() {
-		
+
+	public Card givecard() {
+		Card temp = deck.get(0);
+		deck.remove(0);
+		return temp;
+	}
+
+	public ArrayList<Card> createDeck(int numofDeck) {
+		for (int a = 0; a < numofDeck; a++) {
+			for (int icon = 1; icon < 5; icon++) {
+				int index = 0;
+				String iconMask = "";
+				// 初始化花色、字標
+				if (icon == 1) {
+					index = 0;
+					iconMask = "♥";
+				} else if (icon == 2) {
+					index = 13;
+					iconMask = "♦";
+				} else if (icon == 3) {
+					index = 26;
+					iconMask = "♠";
+				} else if (icon == 4) {
+					index = 39;
+					iconMask = "♣";
+				}
+				for (int i = index; i < index + 13; i++) {
+					if (i == index + 0) {
+						Card c = new Card("A", i - index + 11, icon, iconMask, true);
+						deck.add(c);
+					} else if (i > index && i < 10 + index) {
+						Card c = new Card(String.valueOf(i + 1 - index), i - index + 1, icon, iconMask, true);
+						deck.add(c);
+					} else if (i >= 10 + index && i < 13 + index) {
+
+						if (i == 10 + index) {
+							Card c = new Card("J", 10, icon, iconMask, true);
+							deck.add(c);
+						} else if (i == 11 + index) {
+							Card c = new Card("Q", 10, icon, iconMask, true);
+							deck.add(c);
+						} else if (i == 12 + index) {
+							Card c = new Card("K", 10, icon, iconMask, true);
+							deck.add(c);
+						}
+					}
+				}
+			}
+		}
+
+		return deck;
 	}
 }
-
--- src/v1/Player.java
+++ src/v1/Player.java
@@ -3,26 +3,38 @@ package v1;
 import java.util.ArrayList;
 
 public class Player {
-	private ArrayList<Card> handcard ;
-	
-	
-	public void extraCard() {
-		//叫牌
+	private ArrayList<Card> handcard;
+
+	public void extraCard(Card card) {
+		// 叫牌
+		handcard.add(card);
 	}
 
 	public ArrayList<Card> getHandcard() {
-		//取得手牌
+		// 取得手牌
 		return handcard;
 	}
+
 	public void clearHand() {
-		//清空手牌
+		// 清空手牌
+		handcard.clear();
 	}
-	
+
 	public int handvalue() {
 		// 計算牌值
-		int handvalue = 1;
-		return handvalue;
+		int sum = 0;
+		int ace = 0;
+		for (int j = 0; j < handcard.size(); j++) {
+			sum = sum + handcard.get(j).getValue();
+			if (handcard.get(j).getMask().equals("A")) {
+				ace++;
+			}
+		}
+		if (sum > 21 && ace > 0) {
+			for (int i = 0; sum > 21; i++) {
+				sum = sum - i * 10;
+			}
+		}
+		return sum;
 	}
-	
-	
 }
